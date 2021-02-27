a9abb7841e419f4bfc5691a45d1f49b21f690939
Richie Holsenback <richieholsenback@gmail.com>
2021-02-27 10:12:21 -0600

    better ourflix case study

--- src/Components/Resume/TechStack.js
+++ src/Components/Resume/TechStack.js
@@ -15,26 +15,13 @@ export const TechStack = () => {
                     <ul className="tech-descrip">
                         <li className="tech-descrip-list text-w">Information Architecture</li>
                         <li className="tech-descrip-list text-w">Persona Development</li>
-                        <li className="tech-descrip-list text-w">Wireframing</li>
-                        <li className="tech-descrip-list text-w">Prototyping</li>
-                        <li className="tech-descrip-list text-w">Design Research</li>
+                        <li className="tech-descrip-list text-w">Wireframes, Mockups & Prototyping</li>
+                        <li className="tech-descrip-list text-w">Figma</li>
                         <li className="tech-descrip-list text-w">Journey Mapping</li>
                         <li className="tech-descrip-list text-w">Experience Mapping</li>
                         <li className="tech-descrip-list text-w">Interaction Flows</li>
                     </ul>
                 </Col>
-                <Col s={4} className="tech-stack-division">
-                    <h4 id="h4" className="stack-title text-w">Development</h4>
-                    <ul className="tech-descrip">
-                        <li className="tech-descrip-list text-w">HTML</li>
-                        <li className="tech-descrip-list text-w">CSS</li>
-                        <li className="tech-descrip-list text-w">JavaScript</li>
-                        <li className="tech-descrip-list text-w">ReactJS</li>
-                        <li className="tech-descrip-list text-w">SCSS</li>
-                    </ul>
-                </Col>
-            </Row>
-            <Row id="pad">
                 <Col s={4} className="tech-stack-division">
                     <h4 id="h4" className="stack-title text-w">Design</h4>
                     <ul className="tech-descrip">
@@ -43,6 +30,19 @@ export const TechStack = () => {
                         <li className="tech-descrip-list text-w">Interaction</li>
                         <li className="tech-descrip-list text-w">Content</li>
                         <li className="tech-descrip-list text-w">Application/Interface</li>
+                        <li className="tech-descrip-list text-w">Accessibility</li>
+                    </ul>
+                </Col>
+            </Row>
+            <Row id="pad">
+                <Col s={4} className="tech-stack-division">
+                    <h4 id="h4" className="stack-title text-w">Development</h4>
+                    <ul className="tech-descrip">
+                        <li className="tech-descrip-list text-w">HTML</li>
+                        <li className="tech-descrip-list text-w">CSS</li>
+                        <li className="tech-descrip-list text-w">JavaScript</li>
+                        <li className="tech-descrip-list text-w">ReactJS</li>
+                        <li className="tech-descrip-list text-w">SCSS</li>
                     </ul>
                 </Col>
                 <Col s={4} className="tech-stack-division">
